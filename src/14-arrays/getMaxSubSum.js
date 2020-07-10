d2fda39576c8cbefb9418a139d91fb3dcfa66292
AndrewPerepelkin <avp.tep@rambler.ru>
2020-07-10 19:02:27 +0300

    исправлены мелкие ошибки

--- src/14-arrays/getMaxSubSum.js
+++ src/14-arrays/getMaxSubSum.js
@@ -9,11 +9,11 @@ function getMaxSubSum(arr) {
         if (subSum < 0) subSum = 0;
     }
     return maxSum;
-}
+};
 
 alert(getMaxSubSum([-1, 2, 3, -9]));// = 5 (сумма выделенных)
 alert(getMaxSubSum([2, -1, 2, 3, -9]));// = 6
 alert(getMaxSubSum([-1, 2, 3, -9, 11]));// = 11
 alert(getMaxSubSum([-2, -1, 1, 2]));// = 3
 alert(getMaxSubSum([100, -9, 2, -3, 5]));// = 100
-alert(getMaxSubSum([1, 2, 3]));// = 6 (берём все)
+alert(getMaxSubSum([1, 2, 3])); // = 6 (берём все);
