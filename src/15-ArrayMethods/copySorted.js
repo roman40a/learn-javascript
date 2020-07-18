29e342b7b71e092ce56e6b5a89c7f56a69a883b0
AndrewPerepelkin <avp.tep@rambler.ru>
2020-07-18 11:51:16 +0300

    Добавлено решение задачи copySorted

---
+++ src/15-ArrayMethods/copySorted.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function copySorted(arr) {
+    return arr
+        .slice()
+        .sort()
+};
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted); // CSS, HTML, JavaScript
+alert(arr); // HTML, JavaScript, CSS (без изменений)
