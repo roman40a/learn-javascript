694f2cf60e564d86e351b23e58ab29c285e9e917
AndrewPerepelkin <avp.tep@rambler.ru>
2020-08-20 17:59:44 +0300

    delay

---
+++ src/25-Decorators/delay.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function f(x) {
+    alert(x);
+};
+
+function delay(f, ms) {
+    return function () {
+        setTimeout(() => f.apply(this, arguments), ms);
+    };
+};
+
+// создаём обёртки
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 5000);
+
+f1000("test"); // показывает "test" после 1000 мс
+f1500("test"); // показывает "test" после 1500 мс
