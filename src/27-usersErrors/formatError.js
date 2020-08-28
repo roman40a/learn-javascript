d8f5bf8abd57b076a775e76574a12603f9933ad3
AndrewPerepelkin <avp.tep@rambler.ru>
2020-08-28 13:54:18 +0300

    formatError

---
+++ src/27-usersErrors/formatError.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class FormatError extends SyntaxError {
+    constructor(message) {
+        super(message);
+        this.name = 'FormatError';
+    }
+}
+
+let err = new FormatError("ошибка форматирования");
+
+alert(err.message); // ошибка форматирования
+alert(err.name); // FormatError
+alert(err.stack); // stack
+
+alert(err instanceof FormatError); // true
+alert(err instanceof SyntaxError); // true (потому что наследует от SyntaxError)
