74a0f602422abe0fa1ba36d3617b4e45ed3f79ec
AndrewPerepelkin <avp.tep@rambler.ru>
2020-09-15 13:30:52 +0300

    createCalendare

---
+++ srcDOM/02-changeDocument/createCalendare.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function createCalendare(elem, year, month) {
+    let mon = month - 1;
+
+    let d = new Date(year, mon);
+
+    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+    for (let i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
+    }
+
+    while (d.getMonth() == mon) {
+        table += '<td>' + d.getDate() + '</td>';
+
+        if (getDay(d) % 7 == 6) {
+            table += '</tr><tr>';
+        }
+
+        d.setDate(d.getDate() + 1);
+    }
+
+    if (getDay(d) != 0) {
+        for (let i = getDay(d); i < 7; i++) {
+            table += '<td></td>';
+        } // как это работает???
+    }
+    table += '</tr></table>';
+    console.log(table);
+    elem.innerHTML = table;
+}
+
+function getDay(date) {
+    let day = date.getDay();
+    if (day == 0) day = 7;
+    return day - 1;
+}
+
+createCalendare(calendar, 2020, 9);
