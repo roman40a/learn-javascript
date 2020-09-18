06a1369a96a6f22d7edfd5e36b1b85f466a6370b
AndrewPerepelkin <avp.tep@rambler.ru>
2020-09-18 17:53:09 +0300

    ball in centre of  a field

---
+++ srcDOM/02-changeDocument/size-and-scroll/field.js
@@ -0,0 +1,4 @@
+'use dtrict';
+
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetWidth / 2) + 'px';
