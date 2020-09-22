faaf9ff4c0029197616aa48530ead3e5c10f5621
AndrewPerepelkin <avp.tep@rambler.ru>
2020-09-22 17:04:58 +0300

    positionAt absolute in-out

--- srcDOM/02-changeDocument/coordinates/02-coords/positionAt.js
+++ srcDOM/02-changeDocument/coordinates/02-coords/positionAt.js
@@ -24,19 +24,34 @@ function positionAt(anchor, position, elem) {
     let anchorCoords = getCords(anchor);
 
     switch (position) {
-        case "top":
+        case "top-out":
             elem.style.left = anchorCoords.left + "px";
             elem.style.top = anchorCoords.top - elem.offsetHeight + "px";
             break;
 
-        case "right":
+        case "right-out":
             elem.style.left = anchorCoords.left + anchor.offsetWidth + "px";
             elem.style.top = anchorCoords.top + "px";
             break;
 
-        case "bottom":
+        case "bottom-out":
             elem.style.left = anchorCoords.left + "px";
             elem.style.top = anchorCoords.top + anchor.offsetHeight + "px";
+            break;
+
+        case "top-in":
+            elem.style.left = anchorCoords.left + "px";
+            elem.style.top = anchorCoords.top + "px";
+            break;
+
+        case "right-in":
+            elem.style.left = anchorCoords.left + anchor.offsetWidth - elem.offsetWidth + "px";
+            elem.style.top = anchorCoords.top + "px";
+            break;
+
+        case "bottom-in":
+            elem.style.left = anchorCoords.left + "px";
+            elem.style.top = anchorCoords.top + anchor.offsetHeight - elem.offsetHeight + "px";
             break;
     };
 }
@@ -57,6 +72,10 @@ function showNote(anchor, position, html) {
 // test it
 let blockquote = document.querySelector('blockquote');
 
-showNote(blockquote, "top", "note above");
-showNote(blockquote, "right", "note at the right");
-showNote(blockquote, "bottom", "note below");
+showNote(blockquote, "top-out", "note above");
+showNote(blockquote, "right-out", "note at the right");
+showNote(blockquote, "bottom-out", "note below");
+
+showNote(blockquote, "top-in", "note top-in");
+showNote(blockquote, "right-in", "note right-in");
+showNote(blockquote, "bottom-in", "note bottom-in");
