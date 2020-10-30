ad000fb291a3adc0a332f12438e68a00b9855c47
AndrewPerepelkin <avp.tep@rambler.ru>
2020-10-30 23:00:27 +0300

    02-SmartToolTip

---
+++ srcDOM/07-MovingMouse/02-SmartToolTip/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let tooltip = document.querySelector('#tooltip');
+let elem = document.querySelector('#elem');
+
+setTimeout(function () {
+    new HoverIntent({
+        elem,
+        over() {
+            tooltip.style.left = elem.getBoundingClientRect().left + 5 + 'px';
+            tooltip.style.top = elem.getBoundingClientRect().bottom + 5 + 'px';
+            tooltip.hidden = false;
+        },
+        out() {
+            tooltip.hidden = true;
+        }
+    });
+}, 2000);
