7ebedbb9845d5da59f3a146a8b933cab2e2a3b05
AndrewPerepelkin <avp.tep@rambler.ru>
2020-11-06 16:22:56 +0300

    02-ButtonUp

--- srcDOM/10-Scroll/02-ButtonUp/index.js
+++ srcDOM/10-Scroll/02-ButtonUp/index.js
@@ -1,2 +1,25 @@
 'use strict';
+const btnElem = document.querySelector('#arrowTop');
+btnElem.style.display = 'none';
 
+let winHeight = document.documentElement.clientHeight;
+
+window.addEventListener('scroll', hideBtnUp);
+
+function hideBtnUp() {
+    let windowScroll = window.pageYOffset;
+
+    // btnElem.hidden = (windowScroll < winHeight);
+
+    if (windowScroll > winHeight) {
+        btnElem.style.display = '';
+    } else {
+        btnElem.style.display = 'none';
+    }
+
+    btnElem.addEventListener('click', up)
+
+    function up() {
+        window.scrollTo(pageXOffset, 0);
+    }
+}
